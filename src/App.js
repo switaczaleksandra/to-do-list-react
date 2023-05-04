843b70e0a2117d683dcdea4744b7016001ada34b
Aleksndra Świtacz <switaczaleksandra@gmail.com>
2023-05-04 16:04:17 +0200

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -32,6 +32,13 @@ function App() {
     }));
   }
 
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({
+      ...task, 
+      done: true,
+    })));
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -55,6 +62,7 @@ function App() {
             tasks={tasks}
             hideDone={hideDone}
             toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
           />
         }
       />
